2b98b5be42158f2a165644544830b60db25f1fd9
javiespada <72129909+javiespada@users.noreply.github.com>
2023-02-22 18:26:07 +0100

    Update orderService

--- actividad-backend/zboost/src/main/java/com/inditex/zboost/service/OrderService.java
+++ actividad-backend/zboost/src/main/java/com/inditex/zboost/service/OrderService.java
@@ -2,16 +2,18 @@ package com.inditex.zboost.service;
 
 import com.inditex.zboost.entity.Order;
 import com.inditex.zboost.entity.OrderDetail;
+import com.inditex.zboost.exception.InvalidParameterException;
+import com.inditex.zboost.exception.NotFoundException;
 
 import java.util.Date;
 import java.util.List;
 
 public interface OrderService {
 
-    List<Order> findOrders(int limit);
+    List<Order> findOrders(int limit) throws InvalidParameterException;
 
     List<Order> findOrdersBetweenDates(Date fromDate, Date toDate);
 
-    OrderDetail findOrderDetail(long orderId);
+    OrderDetail findOrderDetail(long orderId) throws NotFoundException;
 
 }
